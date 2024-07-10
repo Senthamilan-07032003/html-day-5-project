8e7d1c913740bd676178a4e3bb987d67b4acc633
Senthamilan-07032003 <thamilansen2003@gmail.com>
2024-07-10 14:37:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('off').addEventListener('click', function() {
+    document.getElementById("image").src = "https://th.bing.com/th?id=OIP.G015oNqM6HUDEP_iA5IWwgAAAA&w=230&h=271&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2";
+});
+
+document.getElementById('on').addEventListener('click', function() {
+    document.getElementById("image").src = "https://th.bing.com/th?id=OIP.VM6NojU3svt85CtR3u4J0AAAAA&w=247&h=252&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2";
+});
